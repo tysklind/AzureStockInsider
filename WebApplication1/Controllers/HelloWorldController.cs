a01ea4bc3fa210cd92985609bc627ef9d1b7c387
tysklind <johan.tysklind@qbit.se>
2015-06-16 23:06:37 +0200

    added link

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // http://www.asp.net/mvc/overview/getting-started/introduction/adding-a-controller
+        // GET: /HelloWorld/ 
+
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
