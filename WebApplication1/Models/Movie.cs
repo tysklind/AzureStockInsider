e65b1e7cc641fc4854e6afcb47b39933e4878387
tysklind <johan.tysklind@qbit.se>
2015-06-18 09:45:30 +0200

    Added a field

--- WebApplication1/Models/Movie.cs
+++ WebApplication1/Models/Movie.cs
@@ -14,6 +14,8 @@ namespace WebApplication1.Models
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+        public string Rating { get; set; }
+
     }
     public class MovieDBContext : DbContext
     {
